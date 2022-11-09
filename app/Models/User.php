58ba02a17368efa4d20734c37b9d481cc1e32025
Antonio Blanco Barreda <46599868+ablancobarreda@users.noreply.github.com>
2022-11-09 16:08:13 -0500

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,8 +2,11 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 /**
  * @property integer $id
@@ -25,6 +28,9 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
  */
 class User extends Authenticatable
 {
+
+    use HasApiTokens, HasFactory, Notifiable;
+
     /**
      * @var array
      */

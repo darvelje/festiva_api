68128f8958408ad01fed33325a3acdc20109abc3
Antonio Blanco Barreda <46599868+ablancobarreda@users.noreply.github.com>
2022-12-12 11:32:08 -0500

    Update SettingsController.php

--- app/Http/Controllers/v1/SettingsController.php
+++ app/Http/Controllers/v1/SettingsController.php
@@ -48,7 +48,7 @@ class SettingsController extends Controller
             //reverse
             $days->add(Carbon::now()->subDays($i)->format('d-m-Y'));
             $ordersTotals->add(Order::whereDate('created_at', '=', Carbon::now()->subDays($i))->count());
-            $ordersCompleted->add(Order::where('status_payment',6)->whereDate('created_at', '=', Carbon::now()->subDays($i))->count());
+            $ordersCompleted->add(Order::where('status',6)->whereDate('created_at', '=', Carbon::now()->subDays($i))->count());
         }
 
         for($i=0; $i<count($ordersTotals); $i++){

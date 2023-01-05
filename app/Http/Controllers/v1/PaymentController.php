493335cf26ddd854c596ebc4c160c7e9c6c68b22
Antonio Blanco Barreda <46599868+ablancobarreda@users.noreply.github.com>
2023-01-05 09:29:22 -0500

    Update PaymentController.php

--- app/Http/Controllers/v1/PaymentController.php
+++ app/Http/Controllers/v1/PaymentController.php
@@ -205,7 +205,7 @@ class PaymentController extends Controller
         return response()->json([
             'code' => 'TEST',
             'message' => 'TEST',
-            'result' => $result['data']['payURL'],
+            'result' => $result['data'],
         ]);
 
         if ($result['error'] == '500') {

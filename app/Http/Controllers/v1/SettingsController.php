ed99bb13367d726296a9fbc70d8b35500577821a
Antonio Blanco Barreda <aablancobarreda@gmail.com>
2023-01-10 15:28:17 -0500

    Update SettingsController.php

--- app/Http/Controllers/v1/SettingsController.php
+++ app/Http/Controllers/v1/SettingsController.php
@@ -410,7 +410,7 @@ class SettingsController extends Controller
 
         if($userDb){
 
-            $walletMoney = ShopsAmounts::with('currency')->sum('amount')->groupBy('currency_id')->get();
+            $walletMoney = ShopsAmounts::sum('amount')->groupBy('currency_id')->get();
 
             return response()->json([
                 'code' => 'test',

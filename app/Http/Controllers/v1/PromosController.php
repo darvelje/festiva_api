804ecd1b0739a8c01d1ff10c94a72c06aca89d38
Royler Marichal <cluzstudio@gmail.com>
2023-01-25 10:10:23 -0500

    fix promo

--- app/Http/Controllers/v1/PromosController.php
+++ app/Http/Controllers/v1/PromosController.php
@@ -100,100 +100,11 @@ class PromosController extends Controller
 
         if($promos){
 
-            $promo1 = [];
-            $promo2 = [];
-            $promo3 = [];
-            $promo4 = [];
-            $promo5 = [];
-            $promo6 = [];
-            $promo7 = [];
-            $promo8 = [];
-            $promo9 = [];
-            $arrayGlobal = [];
-
-            foreach ($promos as $promo){
-                if($promo->promosType->ubication === 'promo1'){
-                    unset($promo->created_at);
-                    unset($promo->updated_at);
-                    unset($promo->category_id);
-                    unset($promo->id_promo_type);
-                    array_push($promo1, $promo);
-                }
-                if($promo->promosType->ubication === 'promo2'){
-                    unset($promo->created_at);
-                    unset($promo->updated_at);
-                    unset($promo->category_id);
-                    unset($promo->id_promo_type);
-                    array_push($promo2, $promo);
-                }
-                if($promo->promosType->ubication === 'promo3'){
-                    unset($promo->created_at);
-                    unset($promo->updated_at);
-                    unset($promo->category_id);
-                    unset($promo->id_promo_type);
-                    array_push($promo3, $promo);
-                }
-                if($promo->promosType->ubication === 'promo4'){
-                    unset($promo->created_at);
-                    unset($promo->updated_at);
-                    unset($promo->category_id);
-                    unset($promo->id_promo_type);
-                    array_push($promo4, $promo);
-                }
-                if($promo->promosType->ubication === 'promo5'){
-                    unset($promo->created_at);
-                    unset($promo->updated_at);
-                    unset($promo->category_id);
-                    unset($promo->id_promo_type);
-                    array_push($promo5, $promo);
-                }
-                if($promo->promosType->ubication === 'promo6'){
-                    unset($promo->created_at);
-                    unset($promo->updated_at);
-                    unset($promo->category_id);
-                    unset($promo->id_promo_type);
-                    array_push($promo6, $promo);
-                }
-                if($promo->promosType->ubication === 'promo7'){
-                    unset($promo->created_at);
-                    unset($promo->updated_at);
-                    unset($promo->category_id);
-                    unset($promo->id_promo_type);
-                    array_push($promo7, $promo);
-                }
-                if($promo->promosType->ubication === 'promo8'){
-                    unset($promo->created_at);
-                    unset($promo->updated_at);
-                    unset($promo->category_id);
-                    unset($promo->id_promo_type);
-                    array_push($promo8, $promo);
-                }
-                if($promo->promosType->ubication === 'promo9'){
-                    unset($promo->created_at);
-                    unset($promo->updated_at);
-                    unset($promo->category_id);
-                    unset($promo->id_promo_type);
-                    array_push($promo9, $promo);
-                }
-
-                unset($promo->promosType);
-
-            }
-            array_push($arrayGlobal, $promo1);
-            array_push($arrayGlobal, $promo2);
-            array_push($arrayGlobal, $promo3);
-            array_push($arrayGlobal, $promo4);
-            array_push($arrayGlobal, $promo5);
-            array_push($arrayGlobal, $promo6);
-            array_push($arrayGlobal, $promo7);
-            array_push($arrayGlobal, $promo8);
-            array_push($arrayGlobal, $promo9);
-
             return response()->json(
                 [
                     'code' => 'ok',
                     'message' => 'Promos',
-                    'promos' => $arrayGlobal
+                    'promos' => $promos
                 ]
             );
         }
@@ -275,7 +186,7 @@ class PromosController extends Controller
     }
 
     //section Update_Promo
-    public function updatePromo(UpdatePromoRequest $request){
+    public function updatePromo(Request $request){
 
         try{
             DB::beginTransaction();
@@ -290,8 +201,6 @@ class PromosController extends Controller
 
                 $promo->status = $request->promoStatus;
                 $promo->url = $request->promoURL;
-                $promo->id_promo_type = $request->promoIdType;
-                $promo->category_id = $request->promoCategoryId;
                 $promo->province_id = $request->promoProvinceId;
 
                 $promo->update();
